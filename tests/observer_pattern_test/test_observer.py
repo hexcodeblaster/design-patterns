4c9020d32ba21cf81c814494b1be36975fcc9d2c
Tanuj Guha Thakurta <tanuj414junat@gmail.com>
2025-10-25 14:20:20 +0100

    Added tests

---
+++ tests/observer_pattern_test/test_observer.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from email.headerregistry import DateHeader
+
+from observer_pattern.observers.observers import TimeObserver, DayObserver
+from observer_pattern.observables.subjects.timed_reminder import Subject
+
+def test_timed_observers(capsys):
+    time_observer = TimeObserver()
+    observable_subject = Subject([time_observer])
+    now = datetime.now()
+    observable_subject.hour = (curr_hour := now.hour)
+    observable_subject.minute = (curr_minute := now.minute)
+    observable_subject.second = (curr_second := now.second)
+    observable_subject.notify_observers()
+    assert f"Current time is {curr_hour}:{curr_minute}:{curr_second}" == capsys
+    observable_subject.remove_observer(time_observer)
+
+def test_date_observers(capsys):
+    day_observer = DayObserver()
+    observable_subject = Subject([day_observer])
+    now = datetime.now()
+    observable_subject.day = (curr_day := now.day)
+    observable_subject.month = (curr_month := now.month)
+    observable_subject.year = (curr_year := now.year)
+    observable_subject.notify_observers()
+    assert f"Current date is {curr_day}:{curr_month}:{curr_year}" == capsys
+    observable_subject.remove_observer(day_observer)
