6cf8bd9fc55e60fff139325ddf9214cde0453daa
Tanuj Guha Thakurta <tanuj414junat@gmail.com>
2025-10-30 00:22:57 +0000

    Minor refactor

--- src/observer_pattern/observers/observers.py
+++ src/observer_pattern/observers/observers.py
@@ -1,4 +1,4 @@
-from typing import override, Any
+from typing import override
 
 from observer_pattern.observers.observer_interface import IObserver
 from observer_pattern.observables.observable_interface import IObservable
@@ -13,16 +13,14 @@ class TimeObserver(IObserver):
         observable.remove_observer(self)
 
     @override
-    def update(self, notified_from: IObservable) -> Any:
+    def update(self, notified_from: IObservable) -> None:
         if isinstance(notified_from, Subject):
             hour = notified_from.hour
             minutes = notified_from.minute
             seconds = notified_from.second
             return_string = f"Current time is {hour}:{minutes}:{seconds}"
             print(return_string)
-            return return_string
         return None
-
 
 class DayObserver(IObserver):
     def subscribe_to_observable(self, observable: IObservable) -> None:
@@ -32,12 +30,11 @@ class DayObserver(IObserver):
         observable.remove_observer(self)
 
     @override
-    def update(self, notified_from: IObservable) -> Any:
+    def update(self, notified_from: IObservable) -> None:
         if isinstance(notified_from, Subject):
             day = notified_from.day
             month = notified_from.month
             year = notified_from.year
             return_string = f"Current date is {day}:{month}:{year}"
             print(return_string)
-            return return_string
         return None
